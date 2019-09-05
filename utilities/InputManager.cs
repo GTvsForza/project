bb5b5ddd05696d46cf3b48f1ae2058fa351e6910
GTvsForza <46792420+GTvsForza@users.noreply.github.com>
2019-09-05 10:50:04 -0600

    Added Script from first Challenge

---
+++ utilities/InputManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class InputManager : MonoBehaviour {
+
+    public Text outText;
+    public InputField textIn;
+    public Button enterButton;
+
+        // Use this for initialization
+        void Start () {
+        outText.text = "";
+	}
+	
+	public void EnterPress ()
+    {
+        outText.text = Utilities.ProcessText(textIn.text);
+    }
+}
---
+++ utilities/Utilites.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utilities {
+
+	public static string ProcessText (string input)
+    {
+        // Determine wether user has input word or number       
+        float result;
+
+        if (float.TryParse(input, out result))
+        {
+            // Return string "number" if user enters number
+            return ("Number");
+        }
+        else
+        {
+            // Return string "word" if user enters word
+            return ("Word");
+        }
+    }
+}
