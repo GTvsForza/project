7a64c6469e90da115a172c8bfb237dac86711763
GTvsForza <46792420+GTvsForza@users.noreply.github.com>
2019-09-12 11:30:44 -0600

    Added Average # Letters with Total Words

--- utilities/InputManager.cs
+++ utilities/InputManager.cs
@@ -1,22 +1,52 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
+public static class Utilities {
 
-public class InputManager : MonoBehaviour {
-
-    public Text outText;
-    public InputField textIn;
-    public Button enterButton;
-
-        // Use this for initialization
-        void Start () {
-        outText.text = "";
-	}
-	
-	public void EnterPress ()
+	public static string ProcessText (string input)
     {
-        outText.text = Utilities.ProcessText(textIn.text);
+        // Determine wether user has input word or number       
+        float result;
+
+        int totalLetters = 0;
+        double number;
+        double numWords = 0;
+        int numNumbers = 0;
+        double avgChar = 0;
+
+
+        try
+        {
+            string[] words = input.Split(' ', '.');
+
+            foreach (var word in words)
+            {
+                totalLetters += word.Length;
+
+                if (float.TryParse(input, out result))
+                {
+                    numNumbers++; 
+                    
+                    // Return string "number" if user enters number
+                    // return ("Number");
+                 }
+                 else
+                 {
+                    numWords++;
+
+                    avgChar = (totalLetters / numWords);
+                    
+                    // Return string "word" if user enters word
+                    // return ("Word");
+                  }
+            }
+
+            return ("" + avgChar + ", " + numWords);
+        }
+        catch
+        {
+            return ("Unable to parse");
+        }
     }
 }
